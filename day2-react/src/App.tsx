4e8a910fc1471e27d7aedaef16ffaedeac208829
Owall <97934878+MayOwall@users.noreply.github.com>
2023-08-11 22:27:15 +0900

    modify : 문구 수정

--- day2-react/src/App.tsx
+++ day2-react/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
           src="https://i.namu.wiki/i/S34D_bvEtXM8tvFPRBzM5oZtC5lUw8QUrrHIH-FAm1eJ_2jYYs7eKiBYWr5TcQ_qzNJf2tc45ncHXC7XYO2O1g.webp"
           alt="시공의 폭풍은, 정말 환상적이야!"
         />
-        <h1>Hello World</h1>
+        <h1>시공의 폭풍은, 정말 환상적이야!</h1>
       </span>
     </div>
   );
