69469d101ef079888c5049f18f488e5d8c088805
Owall <97934878+MayOwall@users.noreply.github.com>
2023-08-19 19:40:27 +0900

    doc : README 수정

--- day5-imageapi/front/index.js
+++ day5-imageapi/front/index.js
@@ -1,8 +1,9 @@
-import { createURL, revokeURL } from "./util.js";
+import { createURL, changeServerImage } from "./util.js";
 
 const button = document.querySelector(".image--button");
 const input = document.querySelector(".image--input");
 const previewBox = document.querySelector(".preview--box");
+const uploadBox = document.querySelector(".upload--box");
 
 const buttonEvent = (e) => {
   e.preventDefault();
@@ -11,12 +12,18 @@ const buttonEvent = (e) => {
 
 const inputEvent = (e) => {
   e.preventDefault();
+
+  // 이미지 파일 가져오기
   const { files } = e.target;
   const file = files[0];
   if (!file) return alert("No Image");
 
+  // 미리보기 이미지 구현
   const tempURL = createURL(file);
   previewBox.style.backgroundImage = `url(${tempURL})`;
+
+  // 서버로 이미지 전송 및 서버 이미지 구현
+  changeServerImage(uploadBox, file, tempURL);
 };
 
 button.addEventListener("click", buttonEvent);
--- day5-imageapi/front/util.js
+++ day5-imageapi/front/util.js
@@ -3,7 +3,37 @@ export const createURL = (file) => {
   return url;
 };
 
+// 현재는 사용되지 않았지만, 메모리 누수 방지를 위해 이미지 해제 작업이 필요하다.
 export const revokeURL = (url) => {
   URL.revokeObjectURL(url);
   return true;
+};
+
+const postImage = async (image) => {
+  const { data } = await fetch(`https://1day1quote-be.fly.dev/image/upload`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    body: image,
+  });
+  return data;
+};
+
+export const changeServerImage = async (dom, file, temp) => {
+  const formData = new FormData();
+  formData.append("image", file);
+
+  console.log("formData :");
+  console.dir(formData);
+  console.log("image in formData: ");
+  console.dir(formData.get("image"));
+
+  try {
+    const { data } = await postImage(formData);
+    console.dir("url : ", data);
+    dom.style.backgroundImage = data.imageURL;
+  } catch {
+    dom.style.backgroundImage = `url(${temp})`;
+  }
 };
